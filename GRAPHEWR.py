10b633ea816a53f02f019ef781bdbc721827ac67
Variation-4 <variation.atlas@gmail.com>
2026-03-18 14:34:36 -0400

    - Various reformatting of existing code - Converted help_string into a global constant - Added class Log to represent a log file - Changed graph function to handle multiple log files - Added file_menu function to allow the user to select multiple files for consideration - Updated main to handle new functions

--- GRAPHEWR.py
+++ GRAPHEWR.py
@@ -1,7 +1,24 @@
+import os
 import matplotlib.pyplot as plt
 import pandas as pd
 import tkinter as tk
 from tkinter import filedialog
+
+#--CONSTANTS--##############################################
+HELP_STRING = ("-- 'f' to set a file to read from\n"
+               "  -'f full' to see full file path\n"
+               "-- '1' for quantity graph\n"
+               "-- '2' for percentage graph\n"
+               "-- 'h' to print this message again\n"
+               "-- 'q' to exit")
+############################################################
+
+#TODO Add documentation
+class Log:
+    def __init__(self, players, filepath, filename):
+        self.players = players
+        self.filepath = filepath
+        self.filename = filename
 
 def read_file(filename: str) -> list[list[str]]:
     """
@@ -50,25 +67,40 @@ def array(size: int, f) -> list[int | float]:
         new_arr.append(f(i))
     return new_arr
 
-def graph(players: dict[str, pd.DatetimeIndex], y_label: str, y_constructor) -> None:
+#TODO Update documentation
+def graph(logs: list[Log], y_label: str, y_constructor) -> None:
     """
     Create a line graph of some statistic against time, categorized by player (each key in the dictionary).
-    :param players: the dictionary of player names and times
+    :param logs:
+    :param selection:
     :param y_label: string to be shown on the y-axis
     :param y_constructor: function by which to plot the data on the y-axis using the DateTimeIndex as a parameter
     :return: None
     """
+    console_clear()
+    print("Select files to graph (e.g. '0 1 3'):")
+    for i in range(len(logs)):
+        print(i, "|", logs[i].filename)
+    selection = input()
+    selection = selection.split(" ")
+
     plt.figure()
 
-    for label, series in players.items():
-        plt.plot(series, y_constructor(series), label=label, drawstyle='steps-post')
-
+    #TODO If there are multiple logs, use relative time instead of fixed
+    for i in selection:
+        log = logs[int(i)]
+        players = log.players
+        for label, series in players.items():
+            plt.plot(series, y_constructor(series), label=label + (" | " + log.filename if len(selection) > 1
+                                                                   else ""), drawstyle='steps-post')
     plt.xlabel("Time")
     plt.ylabel(y_label)
     plt.legend()
     plt.xticks(rotation=45)
 
     plt.show()
+    console_clear()
+    print(HELP_STRING)
 
 def select_file() -> str:
     """
@@ -95,12 +127,62 @@ def select_file() -> str:
     else:
         return ""
 
+#TODO Add documentation
+def file_menu(logs, full, debug: bool = False):
+    while True:
+        console_clear()
+        print("Files selected:\n"
+              "remove [i] | remove the file from consideration\n"
+              "add        | add a new file\n"
+              "q          | close this menu\n")
+        for i in range(len(logs)):
+            print(i, "|", logs[i].filename, ("(" + logs[i].filepath + ")") if full else "")
+        choiceF = input()
+        if choiceF[:6] == "remove":
+            try:
+                logs.pop(int(choiceF[7:]))
+            except ValueError:
+                print("Invalid selection - not a number")
+            except IndexError:
+                print("Invalid selection - invalid index")
+            except Exception as e:
+                print("Unexpected error:", e)
+        elif choiceF == "add":
+            try:
+                file = select_file()
+                print(file)
+                checks = read_file(file)
+                players = format_check_timeline(checks, debug)
+
+                for player in players:
+                    players[player] = pd.to_datetime(players[player])
+
+                filename = file.split("/")[-1]
+                logs.append(Log(players, file, filename))
+                print("File set")
+            except FileNotFoundError:
+                print("File not found")
+            except Exception as e:
+                print("Unexpected Error: ", e)
+        elif choiceF == "q":
+            console_clear()
+            print(HELP_STRING)
+            break
+        else:
+            print("Invalid input")
+
+def console_clear() -> None:
+    """
+    Clears the console window
+    :return: None
+    """
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+#TODO Add more debug stuff
 def main():
     debug = False
-    players = None
-    help_string = ("-- 'f' to set a file to read from\n-- '1' for quantity graph\n-- '2' for percentage graph\n"
-                   "-- 'h' to print this message again\n-- 'q' to exit")
-    print(help_string)
+    logs = []
+    print(HELP_STRING)
     while True:
         choice = input()
         if choice == "debug": # Toggle debug
@@ -110,35 +192,26 @@ def main():
             else:
                 debug = True
                 print("Debug is now true")
-        elif choice == "f": # Select file to read from
-            try:
-                checks = read_file(select_file())
-                players = format_check_timeline(checks, debug)
-
-                for player in players:
-                    players[player] = pd.to_datetime(players[player])
-                print("File set")
-            except FileNotFoundError:
-                print("File not found")
-            except Exception as e:
-                print("Unexpected Error: ", e)
+        elif choice[0] == "f": # Select file to read from
+            file_menu(logs, choice[2:] == "full", debug)
         elif choice == "1": # Quantity graph
-            if players is None:
+            if len(logs) == 0:
                 print("Set a file to read from first")
             else:
-                graph(players, "Amount of Checks", lambda x: array(len(x), lambda y: y))
+                graph(logs, "Amount of Checks", lambda x: array(len(x), lambda y: y))
         elif choice == "2": # Percentage graph
-            if players is None:
+            if len(logs) == 0:
                 print("Set a file to read from first")
             else:
-                graph(players, "Percentage of Presently Completed Checks", lambda x: array(len(x),
+                graph(logs, "Percentage of Presently Completed Checks", lambda x: array(len(x),
                                                                                            lambda y: (y/len(x) * 100)))
         elif choice == "h": # Print help message
-            print(help_string)
+            print(HELP_STRING)
         elif choice == "q": # Quit
             break
         else:
             print("Invalid input")
 
 if __name__ == '__main__':
+    console_clear()
     main()
